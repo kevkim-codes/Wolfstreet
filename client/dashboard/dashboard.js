b0c6a9ed37437236823b21ca1b087890e90c40dd
Kevin Kim <kevink007@gmail.com>
2016-10-17 19:45:44 -0400

    Added more basic functionality, hard coded stock json

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -2,88 +2,65 @@ angular.module('wolfstreet.dashboard', [])
 
 .controller('dashboardController', function($scope, Stocks) {
 
-  $scope.data = {};
+  $scope.data = [];
 
-  $scope.initialMarket = 0;
-
-  $scope.chart = {
-    options: {
-      chart: {
-        type: 'bar'
-      }
-    },
-    series: [{
-      data: [10, 15, 12, 8, 7]
-    }],
-    title: {
-      text: 'Hello'
-    },
-    loading: true
-  }
-});
-
-.controller('myctrl', function($scope) {
-
-  $scope.addPoints = function() {
-    var seriesArray = $scope.highchartsNG.series
-    var rndIdx = Math.floor(Math.random() * seriesArray.length);
-    seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([1, 10, 20])
+  var promiseData = function(stock) {
+    Stocks.lookUp(stock)
+      .then(function(response) {
+        $scope.data.push(response);
+      })
   };
+  promiseData('aapl');
 
-  $scope.addSeries = function() {
-    var rnd = []
-    for (var i = 0; i < 10; i++) {
-      rnd.push(Math.floor(Math.random() * 20) + 1)
-    }
-    $scope.highchartsNG.series.push({
-      data: rnd
-    })
-  }
+  $scope.stocks = $scope.data.join('').split('');
+  console.log($scope.stocks);
 
-  $scope.removeRandomSeries = function() {
-    var seriesArray = $scope.highchartsNG.series
-    var rndIdx = Math.floor(Math.random() * seriesArray.length);
-    seriesArray.splice(rndIdx, 1)
-  }
+  // var initializeLinks = function () {
+  //   Links.getAll()
+  //     .then(function (links) {
+  //       $scope.data.links = links;
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // };
 
-  $scope.options = {
-    type: 'line'
-  }
+  // initializeLinks();
 
-  $scope.swapChartType = function() {
-    if (this.highchartsNG.options.chart.type === 'line') {
-      this.highchartsNG.options.chart.type = 'bar'
-    } else {
-      this.highchartsNG.options.chart.type = 'line'
-    }
-  }
+})
 
-  $scope.highchartsNG = {
+.controller('highchartsController', function($scope, Stocks) {
+  $scope.usaData = [];
+
+  // var promiseData = function () {
+  //   Stocks.usaGraph()
+  //   .then(function(response) {
+  //     $scope.usaData.push(response);
+  //   })
+  // };
+  // promiseData();
+  // console.log($scope.usaData);
+
+  $scope.chartConfig = {
     options: {
       chart: {
-        type: 'bar'
+        type: 'line'
       }
     },
     series: [{
       data: [10, 15, 12, 8, 7]
     }],
     title: {
-      text: 'Hello'
+      text: 'U.S. Stocks'
+    },
+    xAxis: {
+      text: 'Market Cap'
+    },
+    yAxis: {
+      title: {
+        text: 'Exchange rate'
+      }
     },
     loading: false
-  };
-
-});
-
-// var initializeLinks = function () {
-//   Links.getAll()
-//     .then(function (links) {
-//       $scope.data.links = links;
-//     })
-//     .catch(function (error) {
-//       console.error(error);
-//     });
-// };
-
-// initializeLinks();
+  }
 });
--- client/services/services.js
+++ client/services/services.js
@@ -1,51 +1,44 @@
 angular.module('wolfstreet.services', [])
 
 .factory('Stocks', function($http) {
-
-  var lookUp = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://dev.markitondemand.com/Api/v2/Lookup',
-        config: 'application/json'
-      })
-      .then(function(response) {
-        return response.data;
-      });
-  };
-
-  var usaGraph = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://dev.markitondemand.com/Api/v2/InteractiveChart',
-        config: 'application/json'
-      })
-      .then(function(response) {
-        return response.data;
-      });
-  };
-
-  // var getAll = function() {
-  //   return $http({
-  //       method: 'GET',
-  //       url: '/api/stocks'
-  //     })
-  //     .then(function(response) {
-  //       return response.data;
-  //     });
-  // };
-
-  var addOne = function(stock) {
-    return $http({
-      method: 'POST',
-      url: '/api/stocks',
-      data: stock
-    });
-  };
-
   return {
-    lookUp: lookUp,
-    addOne: addOne,
-    usaGraph: usaGraph
-    // getAll: getAll
-  };
+    lookUp: function(symbol) {
+      return $http({
+          method: 'GET',
+          url: 'https://www.google.com/finance/info?q=' + symbol,
+        })
+        .then(function(response) {
+          return response.data;
+        });
+    },
+
+    usaGraph: function() {
+      return $http({
+          method: 'GET',
+          symbol: 'goog',
+          url: 'http://dev.markitondemand.com/Api/v2/InteractiveChart/json',
+        })
+        .then(function(response) {
+          return response.data;
+        });
+    },
+
+    // var getAll = function() {
+    //   return $http({
+    //       method: 'GET',
+    //       url: '/api/stocks'
+    //     })
+    //     .then(function(response) {
+    //       return response.data;
+    //     });
+    // };
+
+      addStock: function(stock) {
+      return $http({
+        method: 'POST',
+        url: '/api/stocks',
+        data: stock
+      });
+    },
+  }
 });
